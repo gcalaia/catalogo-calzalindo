2b9f834c2349470f6f0cfb62ec8d7dba0dfa4cd9
gcalia <gcalaianov@gmail.com>
2025-10-06 18:10:39 -0300

    Eliminar Header duplicado del page

--- app/page.tsx
+++ app/page.tsx
@@ -267,10 +267,7 @@ export default function Home() {
   return (
   <div className="min-h-screen bg-gray-50">
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-      <div className="mb-8">
-        <Header />
-      </div>
-
+      
       {/* Búsqueda */}
       <div className="mb-4">
         {/* ... tu input ... */}
